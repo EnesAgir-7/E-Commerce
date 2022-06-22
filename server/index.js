08ff213867bd80a9bec824bd38a6ee1b0a13e990
Enes AGIR <enes.agir.7@gmail.com>
2022-06-22 14:12:37 +0200

    sign-up

--- server/index.js
+++ server/index.js
@@ -3,12 +3,16 @@ const mongoose = require('mongoose');
 
 const authRouter = require('./routes/auth');
 
+//! .env
 const PORT = 3000;
-const app = express();
 const DB = "mongodb+srv://enes:qweasdzxc123@cluster0.gphgii1.mongodb.net/?retryWrites=true&w=majority"
+const app = express();
 
+//! middleware
+app.use(express.json());
 app.use(authRouter);
 
+//! Database connections
 mongoose.connect(DB).then(()=>{
     console.log('Connected MongoDB');
 }).catch(e=>{
@@ -18,6 +22,6 @@ mongoose.connect(DB).then(()=>{
 // * "0.0.0.0" reason need to specify this ip address is because
 // * android if use localhost won't work
 // * can be accessed from anywhere
-app.listen(PORT,()=>{
+app.listen(PORT, "0.0.0.0",()=>{
     console.log(`connected at port ${PORT}`);
 })
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,12 +1,35 @@
 const express = require('express');
 const User = require('../models/user');
+const bcryptjs = require('bcryptjs');
 
 const authRouter = express.Router();
 
-authRouter.post('/api/signup', (req,res)=>{
-    const {name,email,password}=req.params;
-    //^ it is checking email exist or nor
-    const existingUser = User.findOne({email});
+
+//* Sign Up
+authRouter.post('/api/signup',async (req,res)=>{
+    try {
+        const {name,email,password}=req.body;
+        //^ it is checking email exist or nor
+        const existingUser = await User.findOne({email});
+        if(existingUser){
+            return res.status(400).json({msg:'User with same email already exist'});
+        }
+
+        //! 8 is salt. salt is a random string and when hash a text for add a salt to it
+        //! the hash algo output is no longer predictable to 
+        const hashPassword = await bcryptjs.hash(password, 8);
+
+        let user = new User({
+            email,
+            password: hashPassword,
+            name,
+        })
+        user =await user.save();
+        //^ send data to the client side
+        res.json(user);
+    } catch (er) {
+        res.status(500).json({error:er.message});
+    }
 });
 
 module.exports = authRouter;
