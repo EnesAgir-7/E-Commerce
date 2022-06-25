393012b7fa8b79f0a81cdbde6cb51ee575f080db
Enes AGIR <enes.agir.7@gmail.com>
2022-06-25 13:25:59 +0200

    persisting state 2

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:ea_software/constants/global_variables.dart';
+import 'package:ea_software/features/auth/home/screens/home_screen.dart';
 import 'package:ea_software/features/auth/screens/auth_screen.dart';
 import 'package:ea_software/features/auth/services/auth_service.dart';
 import 'package:ea_software/providers/user_provider.dart';
@@ -26,6 +27,7 @@ class _MyAppState extends State<MyApp> {
   @override
   void initState() {
     super.initState();
+    authService.getUserData(context);
   }
 
   Widget build(BuildContext context) {
@@ -33,7 +35,7 @@ class _MyAppState extends State<MyApp> {
       title: 'EA Software',
       theme: ThemeData(scaffoldBackgroundColor: GlobalVariables.backgroundColor, colorScheme: const ColorScheme.light(primary: GlobalVariables.secondaryColor), appBarTheme: const AppBarTheme(elevation: 0, iconTheme: IconThemeData(color: Colors.black))),
       onGenerateRoute: (settings) => generateRoute(settings),
-      home: AuthScreen(),
+      home: Provider.of<UserProvider>(context).user.token.isNotEmpty ? const HomeScreen():const AuthScreen(),
     );
   }
 }
