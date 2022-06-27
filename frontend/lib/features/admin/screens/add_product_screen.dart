f0a266c2c46e025627cbd282cef030e1783c7c66
Enes AGIR <enes.agir.7@gmail.com>
2022-06-27 11:30:29 +0200

    picking image

--- frontend/lib/features/admin/screens/add_product_screen.dart
+++ frontend/lib/features/admin/screens/add_product_screen.dart
@@ -1,7 +1,11 @@
+import 'dart:io';
+
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:dotted_border/dotted_border.dart';
 import 'package:ea_software/common/custom_button.dart';
 import 'package:ea_software/common/custom_textfield.dart';
 import 'package:ea_software/constants/global_variables.dart';
+import 'package:ea_software/constants/utils.dart';
 import 'package:flutter/material.dart';
 
 class AddProductScreen extends StatefulWidget {
@@ -19,6 +23,8 @@ class _AddProductScreenState extends State<AddProductScreen> {
   final TextEditingController quantityController = TextEditingController();
 
   String category = 'Mobiles';
+  List<File> images = [];
+
   @override
   void dispose() {
     // TODO: implement dispose
@@ -36,6 +42,13 @@ class _AddProductScreenState extends State<AddProductScreen> {
     'Books',
     'Fashion',
   ];
+
+  void selectImage() async {
+    var res = await pickImage();
+    setState(() {
+      images = res;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -64,39 +77,60 @@ class _AddProductScreenState extends State<AddProductScreen> {
                 const SizedBox(
                   height: 20,
                 ),
-                DottedBorder(
-                  borderType: BorderType.RRect,
-                  radius: const Radius.circular(10),
-                  dashPattern: const [10, 6, 4, 6],
-                  strokeCap: StrokeCap.round,
-                  strokeWidth: 2,
-                  child: Container(
-                    width: double.infinity,
-                    height: 150,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        const Icon(
-                          Icons.folder_open,
-                          size: 40,
+                images.isNotEmpty
+                    ? CarouselSlider(
+                        items: images.map(
+                          (i) {
+                            return Builder(
+                              builder: (BuildContext context) => Image.file(
+                                i,
+                                fit: BoxFit.cover,
+                                height: 200,
+                              ),
+                            );
+                          },
+                        ).toList(),
+                        options: CarouselOptions(
+                          viewportFraction: 1,
+                          height: 200,
                         ),
-                        const SizedBox(
-                          height: 15,
-                        ),
-                        Text(
-                          'Select Product Images',
-                          style: TextStyle(
-                            fontSize: 15,
-                            color: Colors.grey.shade400,
+                      )
+                    : GestureDetector(
+                        onTap: selectImage,
+                        child: DottedBorder(
+                          borderType: BorderType.RRect,
+                          radius: const Radius.circular(10),
+                          dashPattern: const [10, 6, 4, 6],
+                          strokeCap: StrokeCap.round,
+                          strokeWidth: 2,
+                          child: Container(
+                            width: double.infinity,
+                            height: 150,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                const Icon(
+                                  Icons.folder_open,
+                                  size: 40,
+                                ),
+                                const SizedBox(
+                                  height: 15,
+                                ),
+                                Text(
+                                  'Select Product Images',
+                                  style: TextStyle(
+                                    fontSize: 15,
+                                    color: Colors.grey.shade400,
+                                  ),
+                                )
+                              ],
+                            ),
                           ),
-                        )
-                      ],
-                    ),
-                  ),
-                ),
+                        ),
+                      ),
                 const SizedBox(
                   height: 30,
                 ),
@@ -135,8 +169,10 @@ class _AddProductScreenState extends State<AddProductScreen> {
                     },
                   ),
                 ),
-                const SizedBox(height: 10,),
-                CustomButton(onTap: (){}, text: 'Sell')
+                const SizedBox(
+                  height: 10,
+                ),
+                CustomButton(onTap: () {}, text: 'Sell')
               ],
             ),
           ),
