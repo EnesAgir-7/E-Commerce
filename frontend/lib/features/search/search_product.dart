31fc47b63a0243c4f494a35ca4dcb23d5f47b395
Enes AGIR <enes.agir.7@gmail.com>
2022-07-03 14:21:46 +0200

    fix stoke

--- frontend/lib/features/search/search_product.dart
+++ frontend/lib/features/search/search_product.dart
@@ -72,7 +72,7 @@ class SearchedProduct extends StatelessWidget {
                       width: 235,
                       padding: const EdgeInsets.only(left: 10, top: 5),
                       child: Text(
-                        '${product.quantity} peace in stoke',
+                        '${product.quantity.toStringAsFixed(0)} piece in stoke',
                         style: const TextStyle(
                           color: Colors.teal,
                         ),
